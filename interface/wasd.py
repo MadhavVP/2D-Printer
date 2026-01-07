6fbb9ef584b1d44b7faf8e86389630d14c5c891a
MadhavVP <vpmadhav@gmail.com>
2026-01-07 01:30:31 -0600

    minor tweaks to wasd and tuning of steprate

--- interface/wasd.py
+++ interface/wasd.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import sys
 import tty
 import termios
-import time
+from time import sleep
 
 def getc():
     fd = sys.stdin.fileno()
@@ -15,25 +15,35 @@ def getc():
     return ch
 
 def main():
-    gpio_outs = [17, 27, 22, 23]
-    gpio_map = {'a':[17, 27, GPIO.HGIH], 'd':[17, 27, GPIO.LOW], 'w':[22, 23, GPIO.HIGH], 's':[22, 23, GPIO.LOW]}
+    gpio_outs = [17, 27, 22, 23, 2, 3, 4]
+    gpio_map = {'a':[17, 27, GPIO.HIGH], 'd':[17, 27, GPIO.LOW], 'w':[22, 23, GPIO.HIGH], 's':[22, 23, GPIO.LOW]}
     dir_map = {'a':'left', 'd':'right', 'w':'up', 's':'down'}
+    ms1 = 4
+    ms2 = 3
+    ms3 = 2
 
     GPIO.setmode(GPIO.BCM)
     for x in gpio_outs:
         GPIO.setup(x, GPIO.OUT)
 
+    GPIO.output(ms1, GPIO.HIGH)
+    GPIO.output(ms2, GPIO.HIGH)
+    GPIO.output(ms3, GPIO.HIGH)
+
     while True:
         c = getc()
         if c == 'q':
+            for x in gpio_outs:
+                GPIO.output(x, GPIO.LOW)
             break
         if c in gpio_map:
             GPIO.output(gpio_map[c][1], gpio_map[c][2])
-            GPIO.output(gpio_map[c][0], GPIO.HIGH)
-            print(f'{dir_map[c]}')
-            sleep(0.01)
-            GPIO.output(gpio_map[c][0], GPIO.LOW)
-            GPIO.output(gpio_map[c][1], GPI.LOW)
+            for i in range(1600):
+                GPIO.output(gpio_map[c][0], GPIO.HIGH)
+                #print(f'{dir_map[c]}')
+                sleep(0.0005)
+                GPIO.output(gpio_map[c][0], GPIO.LOW)
+            GPIO.output(gpio_map[c][1], GPIO.LOW)
 
 if __name__ == "__main__":
     main()
