16993936b7e1d45136bfbf24152925de4b5bac23
MadhavVP <vpmadhav@gmail.com>
2026-01-10 01:38:37 -0600

    extrusion microstep tweaks and add limit switches to wasd code

--- interface/constants.py
+++ interface/constants.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 
 gpio_outs = [17, 27, 22, 23, 2, 3, 4]
 gpio_map = {'a':[17, 27, GPIO.HIGH], 'd':[17, 27, GPIO.LOW], 'w':[22, 23, GPIO.HIGH], 's':[22, 23, GPIO.LOW],
-            'p':[3, 2, GPIO.HIGH], 'u':[3, 2, GPIO.LOW]}
+            'p':[3, 2, GPIO.LOW], 'u':[3, 2, GPIO.HIGH]}
 dir_map = {'a':'left', 'd':'right', 'w':'up', 's':'down', 'p':'pencil down', 'u':'pencil up'}
 allms = 4
 #ms2 = 3
@@ -10,4 +10,4 @@ allms = 4
 xstop = 21
 ystop = 20
 stepspeed = 0.00005
-penoffset = 200
+penoffset = 96
--- interface/wasd.py
+++ interface/wasd.py
@@ -3,7 +3,7 @@ import sys
 import tty
 import termios
 from time import sleep
-from constants import gpio_map, gpio_outs, dir_map, allms, penoffset
+from constants import gpio_map, gpio_outs, dir_map, allms, penoffset, xstop, ystop
 
 def getc():
     fd = sys.stdin.fileno()
@@ -23,6 +23,9 @@ def main():
 
     GPIO.output(allms, GPIO.HIGH)
 
+    GPIO.setup(xstop, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(ystop, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
     while True:
         c = getc()
         if c == 'q':
@@ -37,9 +40,24 @@ def main():
                 steps = penoffset
                 slptime = 0.005
             for i in range(steps):
-                GPIO.output(gpio_map[c][0], GPIO.HIGH)
-                sleep(slptime)
-                GPIO.output(gpio_map[c][0], GPIO.LOW)
+                if GPIO.input(ystop) and GPIO.input(xstop):
+                    GPIO.output(gpio_map[c][0], GPIO.HIGH)
+                    sleep(slptime)
+                    GPIO.output(gpio_map[c][0], GPIO.LOW)
+                if not GPIO.input(xstop):
+                    GPIO.output(gpio_map['a'][1], gpio_map['a'][2])
+                    while not GPIO.input(xstop):
+                        GPIO.output(gpio_map['a'][0], GPIO.HIGH)
+                        sleep(slptime)
+                        GPIO.output(gpio_map['a'][0], GPIO.LOW)
+                if not GPIO.input(ystop):
+                    GPIO.output(gpio_map['w'][1], gpio_map['w'][2])
+                    while not GPIO.input(ystop):
+                        GPIO.output(gpio_map['w'][0], GPIO.HIGH)
+                        sleep(slptime)
+                        GPIO.output(gpio_map['w'][0], GPIO.LOW)
+                
+
             GPIO.output(gpio_map[c][1], GPIO.LOW)
             print(f'{dir_map[c]}')
 
